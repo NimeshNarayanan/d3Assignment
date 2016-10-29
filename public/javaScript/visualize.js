d8d26fc284f8bd49d53c6fc5e3b2abaa020b135a
NimeshNarayanan <nimeshp@thoughtworks.com>
2016-10-29 13:01:45 +0530

    changed data from number to object of values and keys for making the new data unique from old one.

--- public/javaScript/visualize.js
+++ public/javaScript/visualize.js
@@ -4,42 +4,48 @@ var translate = function(x, y) {
     return `translate(${ x },${ y })`;
 };
 
-var color = d3.scaleOrdinal().domain([1,50,100]).range(['#862d2d','#d27979','#e6b3b3','#ac3939'])
+var color = d3.scaleOrdinal().domain([1,100]).range(['gray','darkcyan ','steelblue'])
 //generating random numbers......................
 
 var generate_random_number = function(data1) {
     var newData = [];
     for (i = 0; i < 10; i++)
-        newData.push(Math.floor((Math.random() * 100)+1));
+        newData.push(generateUniqueKey(i));
     return newData;
 }
 
 var initializeChart = function(data) {
     var bars = d3.select('.container').selectAll('div')
 							.classed('old',true)
-							// .style('background-color','steelblue')
-							.data(data,function (d) {return d;});
+							.data(data,function (d) {return d.key;});
     var new_bar = bars.enter().append('div')
         .classed('rect', true)
-        .style('background-color', color)
-				.text(function(d){return d;})
+				.text(function(d){return d.value;})
 				.style('text-align', 'right')
         .style('width', function(d) {
-            return d * 10 + "px"
+            return d.value * 10 + "px"
         })
-        .style('height', 30 + 'px');
+        .style('height', 30 + 'px')
+				.style('background-color', function(d){
+					return color(d.value)
+				});
 		bars.exit().remove();
 
 }
 
-var generateUniqueKey = function(number) {
-	var key;
+var generateUniqueKey = function(index) {
+	var key = {};
+	index = index?index:Math.floor((Math.random() * 100)+1);
+	key['value'] = Math.floor((Math.random() * 100)+1);
+	key['key'] = Math.floor((Math.random() * 100)+index)
+	return key;
+
 }
 var loadChart = function(){
 	data = generate_random_number();
 	setInterval(function(){
         data.shift();
-        data.push(Math.floor((Math.random()*100)));
+        data.push(generateUniqueKey());
 				initializeChart(data)
     },1000)
 }
